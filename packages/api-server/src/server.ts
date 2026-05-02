f1111e2f7167df541daa4106fb212ec8daddbef9
echska <echska0109@gmail.com>
2026-05-02 19:27:26 +0300

    Fix TypeScript types for Vercel handler

--- packages/api-server/src/server.ts
+++ packages/api-server/src/server.ts
@@ -1,3 +1,5 @@
-export default function handler(req, res) {
+import { VercelRequest, VercelResponse } from "@vercel/node";
+
+export default function handler(req: VercelRequest, res: VercelResponse) {
   res.status(200).send("API is working!");
 }
